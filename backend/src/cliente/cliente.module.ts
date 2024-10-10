85fe32000538a005efe1b7a9220b13292ceb90e4
r0bBy001 <robby863401@gmail.com>
2024-10-10 02:42:23 -0500

    Asignacion de relacion tipoDoc y tipoCliente con tipo-general

--- backend/src/cliente/cliente.module.ts
+++ backend/src/cliente/cliente.module.ts
@@ -3,9 +3,10 @@ import { ClienteService } from './cliente.service';
 import { ClienteController } from './cliente.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cliente } from './entities/cliente.entity';
+import { TipoGeneral } from 'src/tipo-general/entities/tipo-general.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cliente])],
+  imports: [TypeOrmModule.forFeature([Cliente, TipoGeneral])],
   controllers: [ClienteController],
   providers: [ClienteService],
   exports: [TypeOrmModule],
