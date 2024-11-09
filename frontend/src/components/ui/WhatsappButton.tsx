df953bc1f435bdb4a5625a552031c55f5d4f2132
zsalazarq <salazarsergio1082@gmail.com>
2024-11-09 11:07:34 -0500

    Corrigiendo el error del boton de whatsapp

--- frontend/src/components/ui/WhatsappButton.tsx
+++ frontend/src/components/ui/WhatsappButton.tsx
@@ -6,7 +6,7 @@ function WhatsAppButton() {
   return (
     <a
       className="fixed bottom-5 right-6 p-4 bg-[#2e6329] rounded-full z-10 transition-transform duration-500 ease-in-out hover:scale-110 animate-efecto"
-      href={`https://wa.me/${number}?text=Hola, quisiera realizar una consulta`}
+      href={`https://wa.me/+51946899196?text=Hola Wonder Clouds, quisiera realizar una consulta`}
       target="_blank"
       rel="noopener noreferrer"
     >
