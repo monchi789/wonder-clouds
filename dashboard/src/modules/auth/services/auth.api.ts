200ee219fcdc2f04165d2637b70263b29a82682a
zsalazarq <salazarsergio1082@gmail.com>
2024-11-16 00:34:53 -0500

    Arreglando el login

--- dashboard/src/modules/auth/services/auth.api.ts
+++ dashboard/src/modules/auth/services/auth.api.ts
@@ -1,12 +1,12 @@
 import axios from "axios"
 
-export const getTokenAuth = async (username: string, password: string) => {
+export const getTokenAuth = async (email: string, contrasena: string) => {
   const apiUrl = import.meta.env.VITE_API_URL;
 
   try {
     const res = await axios.post(`${apiUrl}api/v1/auth/login`, {
-      username: username,
-      password: password
+      email: email,
+      contrasena: contrasena
     });
 
     const token = res.data.token;
